b1d57188e014354e9e3b66ec888e634b4a3cfb46
lmgk11 <lulle.gillbergkling@gmail.com>
2017-08-18 21:52:01 +0200

    Update binTree.cs

--- binTree.cs
+++ binTree.cs
@@ -1,94 +1,84 @@
 ﻿using System;
 
-namespace binTree
+class binTree
 {
-    class binTree
-    {
-        public Node root;
+	public Node root;
 
-        public binTree()
+	public binTree()
+	{
+		root = null;
+	}
+
+	public void put(string x)
+	{
+		this.root = addToTree(this.root, x);
+	}
+
+	public bool contains(string x)
+	{
+		return exists(root, x);
+	}
+
+	public void write()
+	{
+		printOut(this.root);
+	}
+
+	public void printOut(Node root)
         {
-            root = null;
-        }
+		if(root != null)
+		{
+			printOut(root.left);
+			Console.WriteLine(root.value);
+			printOut(root.right);
+		}
+	}
 
-        public void put(string x)
-        {
-            this.root = addToTree(this.root, x);
-        }
+	public static bool exists(Node root, string value)
+	{
+		if(root != null)
+		{
+			int c = string.Compare(root.value, value);
 
-        public bool contains(string x)
-        {
-            return exists(root, x);
-        }
+			if (c == 0)
+			{
+				return true;
+        	} if (c < 0)
+			{
+				return exists(root.right, value);
+        	} if (c > 0)
+        	{                    
+				return exists(root.left, value);
+			}
+		} else	
+		{
+			return false;
+		}
+		return false;
+	}
 
-        public void write()
-        {
-            printOut(this.root);
-        }
-
-        public void printOut(Node root)
-        {
-            if(root != null)
-            {
-                printOut(root.left);
-                Console.WriteLine(root.value);
-                printOut(root.right);
-            }
-        }
-
-        public static bool exists(Node root, string value)
-        {
-            if(root != null)
-            {
-                int c = string.Compare(root.value, value);
-
-                if (c == 0)
-                {
-                    return true;
-                } if (c < 0)
-                {
-                    //right
-                    return exists(root.right, value);
-                } if (c > 0)
-                {
-                    //left
-                    
-                    return exists(root.left, value);
-                }
-            } else
-            {
-                return false;
-            }
-
-            return false;
-        }
-
-        public static Node addToTree(Node root, string value)
-        {
+	public static Node addToTree(Node root, string value)
+	{
             
 
-            if(root == null)
-            {
-                return new Node(value);
-            } if(exists(root, value))
-            {
-                return root;
-            }
-            int c = string.Compare(root.value, value);
-            //Console.WriteLine(c);
-            if (c > 0)
-            {
-                root.left = addToTree(root.left, value);
-                return root;
-            } if ( c < 0)
-            {
-                root.right = addToTree(root.right, value);
-                return root;
-            }
-
-            return null;
+        if(root == null)
+        {
+        	return new Node(value);
+        } if(exists(root, value))
+        {
+        	return root;
         }
-    }
+        int c = string.Compare(root.value, value);
+        if (c > 0)
+        {
+        	root.left = addToTree(root.left, value);
+            return root;
+        } if ( c < 0)
+        {
+			root.right = addToTree(root.right, value);
+			return root;
+		}
 
-    
+		return null;
+	}
 }
