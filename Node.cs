107bdb523d5b5b8992383b8eb4e2220323c9d350
lmgk11 <lulle.gillbergkling@gmail.com>
2017-08-18 21:40:22 +0200

    Bintree class And Node Class

---
+++ Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace binTree
+{
+    
+    class Node
+    {
+        public Node left, right;
+        public string value;
+        public Node(string value)
+        {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+
+        
+    }
+}
---
+++ binTree.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace binTree
+{
+    class binTree
+    {
+        public Node root;
+
+        public binTree()
+        {
+            root = null;
+        }
+
+        public void put(string x)
+        {
+            this.root = addToTree(this.root, x);
+        }
+
+        public bool contains(string x)
+        {
+            return exists(root, x);
+        }
+
+        public void write()
+        {
+            printOut(this.root);
+        }
+
+        public void printOut(Node root)
+        {
+            if(root != null)
+            {
+                printOut(root.left);
+                Console.WriteLine(root.value);
+                printOut(root.right);
+            }
+        }
+
+        public static bool exists(Node root, string value)
+        {
+            if(root != null)
+            {
+                int c = string.Compare(root.value, value);
+
+                if (c == 0)
+                {
+                    return true;
+                } if (c < 0)
+                {
+                    //right
+                    return exists(root.right, value);
+                } if (c > 0)
+                {
+                    //left
+                    
+                    return exists(root.left, value);
+                }
+            } else
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public static Node addToTree(Node root, string value)
+        {
+            
+
+            if(root == null)
+            {
+                return new Node(value);
+            } if(exists(root, value))
+            {
+                return root;
+            }
+            int c = string.Compare(root.value, value);
+            //Console.WriteLine(c);
+            if (c > 0)
+            {
+                root.left = addToTree(root.left, value);
+                return root;
+            } if ( c < 0)
+            {
+                root.right = addToTree(root.right, value);
+                return root;
+            }
+
+            return null;
+        }
+    }
+
+    
+}
